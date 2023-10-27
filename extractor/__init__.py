2f7f1f78a0edc5450184fdb96c15623ed806ffd0
Poleric <nixonleongkb@gmail.com>
2023-10-28 01:49:10 +0800

    logging

--- extractor/__init__.py
+++ extractor/__init__.py
@@ -1,8 +1,12 @@
 import re
+import logging
 from extractor.exceptions import ExtractorError
 from extractor.twitter import TWITTER_REGEX, save_tweet_media
 from extractor.pixiv import PIXIV_REGEX, save_pixiv_media
 from extractor.dcinside import DCINSIDE_REGEX, save_dcinside_media
+
+
+logger = logging.getLogger(__name__)
 
 
 class NotValidQuery(ExtractorError):
@@ -29,12 +33,15 @@ async def save_media(query: str, *, use_twitter=True, use_pixiv=True, use_dcinsi
     """
 
     if use_twitter and re.match(TWITTER_REGEX, query):
+        logger.info("Saving Twitter media.")
         await save_tweet_media(query, **kwargs)
         return
     if use_pixiv and re.match(PIXIV_REGEX, query):
+        logger.info("Saving Pixiv media.")
         await save_pixiv_media(query, **kwargs)
         return
     if use_dcinside and re.match(DCINSIDE_REGEX, query):
+        logger.info("Saving Dcinside media.")
         await save_dcinside_media(query, **kwargs)
         return
     raise NotValidQuery("The specified query type is not supported.")
