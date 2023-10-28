65f7c84d0b06fe195259bf28e84574fe9688c911
Poleric <nixonleongkb@gmail.com>
2023-10-28 13:13:28 +0800

    fix on_message triggering when invoking a command
    - also added a shit lot of loggings
    - general refactoring

--- bot.py
+++ bot.py
@@ -12,12 +12,14 @@ from extractor import save_media, NotValidQuery
 from extractor.exceptions import ScrapingException, MediaNotFound
 
 
-URL_REGEX = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
 BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 if not BOT_TOKEN:
-    logging.exception("DISCORD_BOT_TOKEN is required. Set the bot token as an environment variable with the key DISCORD_BOT_TOKEN")
-    sys.exit()
+    logging.exception("DISCORD_BOT_TOKEN is required. "
+                      "Set the bot token as an environment variable with the key `DISCORD_BOT_TOKEN`")
+    sys.exit(1)
 
+
+URL_REGEX = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
 discord.utils.setup_logging()
 bot = commands.Bot(
     command_prefix=".",
@@ -30,12 +32,18 @@ bot = commands.Bot(
 
 @bot.listen()
 async def on_message(msg: Message):
+    ctx: Context = await bot.get_context(msg)
+    if ctx.valid:
+        return
+
     url = re.search(URL_REGEX, msg.content)  # might be a url
     if url:
         try:
             await save_media(url[0])
+        except NotValidQuery:
+            logging.exception(f"{url=} is not supported.")
         except MediaNotFound:
-            logging.exception(f"Media is not found {url}.")
+            logging.exception(f"Media is not found in {url}.")
         except ScrapingException:
             logging.exception(f"Error encountered when saving {url}")
         else:  # no errors
@@ -49,15 +57,19 @@ async def save(ctx: Context, msg: Message):
     try:
         await save_media(url)
     except NotValidQuery:
-        logging.exception(f"Saving {url} is not supported yet.")
+        logging.exception(f"{url=} is not supported.")
+        await ctx.reply(f"Saving {url} is not supported yet.")
         await msg.add_reaction("❓")
     except MediaNotFound:
-        logging.exception(f"Media is not found {url}.")
+        logging.exception(f"Media is not found in {url}.")
+        await ctx.reply(f"No media is found for the url {url}")
         await msg.add_reaction("❌")
-    except ScrapingException:
+    except ScrapingException:  # so far should not be ran since theres no ScrapingException raises
         logging.exception(f"Error encountered when saving {url}")
+        await ctx.reply(f"Error encountered when saving url {url}")
         await msg.add_reaction("❌")
     else:  # no errors
+        await ctx.message.add_reaction("✅")
         await msg.add_reaction("✅")
 
 
