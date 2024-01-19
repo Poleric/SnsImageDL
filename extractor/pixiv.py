6a8bdb7cf8277fec6c8861130ec4512d478b3d69
Poleric <nixonleongkb@gmail.com>
2024-01-19 16:05:02 +0800

    Overhaul progress - pixiv
    Made most functions asynchronous

--- extractor/pixiv.py
+++ extractor/pixiv.py
@@ -1,101 +1,141 @@
+import asyncio
+import os
+import re
+import functools
+from typing import Iterable
+
 from pixivpy3 import AppPixivAPI
 from pixivpy3.utils import PixivError
-import re
-import os
-import logging
-import sys
+from extractor.base import Extractor, UrlLike, PathLike
+from extractor.exceptions import MediaNotFound, InvalidLink
 
-from extractor.exceptions import MediaNotFound
-
-from typing import Union
-from os import PathLike
-PathLike = Union[str, bytes, PathLike]
+# from typing import override
 
 
-logger = logging.getLogger(__name__)
-PIXIV_REGEX = r"https://www.p.?ixiv.net/en/artworks/([0-9]+)"
-REFRESH_TOKEN = os.getenv("PIXIV_REFRESH_TOKEN")
-if not REFRESH_TOKEN:
-    logging.exception("PIXIV_REFRESH_TOKEN is required. Refer to https://gist.github.com/ZipFile/c9ebedb224406f4f11845ab700124362 to get your refresh token and add it into the environment variable with the name PIXIV_REFRESH_TOKEN")
-    sys.exit()
+class NotPixivLink(InvalidLink):
+    pass
 
 
-def get_pixiv_id(url: str) -> str:
-    res = re.search(PIXIV_REGEX, url)
-    assert res, f"{url} is not a Pixiv url"
-    return res[1]
+class Pixiv(Extractor):
+    SITE_REGEX = r"https://www.p.?ixiv.net/.*artworks/([0-9]+)"
+    API_ARGS = {
+        "timeout": 3
+    }
+    session: AppPixivAPI
 
+    def __init__(self, refresh_token: str = None):
+        super().__init__()
 
-def get_pixiv_illust(session: AppPixivAPI, url: str) -> dict:
-    # tries 3 times
-    illust_id = get_pixiv_id(url)
-    for i in range(3):
-        try:
-            json_result = session.illust_detail(illust_id)
-        except TimeoutError:
-            logger.warning(f"Timeout when retrieving illustration {illust_id}")
-        else:
-            if "error" in json_result and "invalid_grant" in json_result["error"]["message"]:
-                session.auth()
-                continue
-            return json_result.illust
+        self.loop = asyncio.get_running_loop()
+        self.REFRESH_TOKEN = refresh_token or os.getenv("PIXIV_REFRESH_TOKEN")
+        assert self.REFRESH_TOKEN, "PIXIV_REFRESH_TOKEN is required. Refer to https://gist.github.com/ZipFile/c9ebedb224406f4f11845ab700124362 to get your refresh token and add it into the environment variable with the name PIXIV_REFRESH_TOKEN"
 
-    # if no results
-    raise PixivError(f"Error in retrieving {url} data")
+    # @override
+    def __str__(self):
+        return "Pixiv"
 
+    # @override
+    async def __aenter__(self):
+        self.session = await self.loop.run_in_executor(
+            None,
+            functools.partial(AppPixivAPI, **self.API_ARGS)
+        )
+        self.session.auth(refresh_token=self.REFRESH_TOKEN)
+        return self
 
-def get_pixiv_media_urls(data: dict) -> list[str]:
-    urls = []
+    # @override
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        return
 
-    # is illustration
-    if data["type"] == "illust":
-        if data["meta_pages"]:  # for multiple images stuff
-            for page in data["meta_pages"]:
-                urls.append(page["image_urls"]["original"])
-        else:
-            urls.append(data["meta_single_page"]["original_image_url"])
-    # is animation
-    elif data["type"] == "ugoira":
-        # TODO: related issues: https://github.com/upbit/pixivpy/issues/164, https://github.com/upbit/pixivpy/issues/253
-        raise NotImplemented("Ugoira is a bit iffy to make. will do sometime")
-    # is nothing
-    else:
-        raise MediaNotFound
+    # @override
+    def get_filename(self, source_url: UrlLike):
+        return
 
-    return urls
+    # @override
+    async def retrieve_media_urls(self, webpage_url: UrlLike) -> Iterable[str]:
+        return self.get_pixiv_media_urls(
+            await self.get_pixiv_illust(webpage_url)
+        )
 
+    # @override
+    async def save(self, webpage_url: UrlLike, output_directory: PathLike) -> None:
+        os.makedirs(output_directory, exist_ok=True)
 
-async def save_pixiv_media(url, out_dir: PathLike = "./pixiv_media"):
-    pixiv_session = AppPixivAPI(timeout=3)
-    pixiv_session.auth(refresh_token=REFRESH_TOKEN)
+        for media_url in await self.retrieve_media_urls(webpage_url):
+            try:
+                self.loop.run_in_executor(
+                    None,
+                    functools.partial(
+                        self.session.download,
+                        url=media_url,
+                        path=output_directory
+                    )
+                )
+            except PixivError:
+                self.logger.exception(f"Download {media_url} failed.")
 
-    os.makedirs(out_dir, exist_ok=True)
+    def get_pixiv_id(self, webpage_url: str) -> str:
+        res = re.search(self.SITE_REGEX, webpage_url)
+        if not res:
+            raise NotPixivLink
+        return res[1]
 
-    illust = get_pixiv_illust(pixiv_session, url)
-    img_urls = get_pixiv_media_urls(illust)
-
-    # tries 3 times
-    for img_url in img_urls:
+    async def get_pixiv_illust(self, webpage_url: str) -> dict:
+        # tries 3 times
+        illust_id = self.get_pixiv_id(webpage_url)
         for i in range(3):
             try:
-                if pixiv_session.download(img_url, path=out_dir):
-                    pass
-                break
-            except PixivError:
-                logging.exception(f"Download {url} failed.")
+                json_result = await self.loop.run_in_executor(
+                    None,
+                    self.session.illust_detail,
+                    illust_id
+                )
+            except TimeoutError:
+                self.logger.warning(f"Timeout when retrieving illustration {illust_id}")
+            else:
+                if "error" in json_result and "invalid_grant" in json_result["error"]["message"]:
+                    self.session.auth()
+                    continue
+                return json_result.illust
+
+        # if no results
+        raise PixivError(f"Error in retrieving {webpage_url} data")
+
+    @staticmethod
+    def get_pixiv_media_urls(data: dict) -> list[str]:
+        media_urls = []
+
+        match data:
+            case {
+                "type": "illust",
+                "meta_single_page": page,
+                "meta_pages": []
+            }:
+                media_urls.append(page["original_image_url"])
+            case {
+                "type": "illust",
+                "meta_single_page": {},
+                "meta_pages": pages
+            }:
+                for page in pages:
+                    media_urls.append(page["image_urls"]["original"])
+            case {
+                "type": "ugoira"
+            }:
+                # TODO: related issues: https://github.com/upbit/pixivpy/issues/164, https://github.com/upbit/pixivpy/issues/253
+                raise NotImplemented("Ugoira is a bit iffy to make. will do sometime")
+            case _:
+                raise MediaNotFound
+
+        return media_urls
 
 
 if __name__ == "__main__":
-    import asyncio
-    urls = [
-        "https://www.pixiv.net/en/artworks/97138089",
-        "https://www.pixiv.net/en/artworks/92217855",
-        "https://www.pixiv.net/en/artworks/99875723",
-        "https://www.pixiv.net/en/artworks/96398887",
-    ]
+    single_illust = "https://www.pixiv.net/en/artworks/97138089"
+    multi_illust = "https://www.pixiv.net/en/artworks/115161963"
 
     async def main():
-        await asyncio.gather(
-            *[save_pixiv_media(url) for url in urls]
-        )
+        async with Pixiv() as pixiv:
+            await pixiv.save(multi_illust, "./pixiv_media")
+
     asyncio.run(main())
