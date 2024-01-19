4d518be28e451669c7669153ab751e120ae94c7b
Poleric <nixonleongkb@gmail.com>
2024-01-19 21:25:53 +0800

    overhaul - dcinside
    also fixed scraping issue where finding element with source url by targeting a tag with specific loading image url ("https://nstatic.dcinside.com/dc/m/img/dccon_loading_nobg200.png")
    - loading image now use different url.
    - now instead looks for `img` tag with class of only "lazy" and have attribute "data-original"

--- extractor/dcinside.py
+++ extractor/dcinside.py
@@ -1,81 +1,83 @@
-from bs4 import BeautifulSoup
-import aiohttp
 import os
-import logging
 import re
+import aiohttp
+from bs4 import BeautifulSoup
+from extractor.base import Extractor, UrlLike, PathLike
+from extractor.exceptions import MediaNotFound, InvalidLink, SessionNotCreated
 
-from extractor.exceptions import MediaNotFound, InvalidLink
-from extractor.utils import download_source_url
-
-from aiohttp import ClientSession
-from typing import Union, Iterable
-from os import PathLike
-PathLike = Union[str, bytes, PathLike]
-logger = logging.getLogger(__name__)
+from typing import Iterable, Mapping
 
 
 class NotDcinsideLink(InvalidLink):
     pass
 
 
-DCINSIDE_REGEX = r"https://(?:gall|m).dcinside.com/[a-zA-Z/]*(?:\?id=|(?:board|m)/)(\w+)(?:&no=|/)(\d+)"
-DCINSIDE_URL = r"https://m.dcinside.com/board/{}/{}"
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36"
-}
-GALLERY_POSTS_COOKIES = {
-    "__gat_mobile_search": "1",
-    "list_count": "200",
-}
+class Dcinside(Extractor):
+    SITE_REGEX = r"https://(?:gall|m).dcinside.com/[a-zA-Z/]*(?:\?id=|(?:board|m)/)(\w+)(?:&no=|/)(\d+)"
+    SCRAPE_ENDPOINT = r"https://m.dcinside.com/board/{}/{}"
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36"
+    }
+    # IMAGE_SOURCE_REGEX = r"https://dcimg1.dcinside.com/viewimage.php?id=.+"
 
+    # @override
+    def __str__(self):
+        return "dcinside"
 
-def get_board_post_id(url: str) -> tuple[str, str]:
-    res = re.search(DCINSIDE_REGEX, url)
-    if not res:
-        raise NotDcinsideLink(f"{url} is not a dcinside link.")
-    return res[1], res[2]
+    # @override
+    async def save(self, webpage_url: UrlLike, output_directory: PathLike = "./dcinside_media", filename: str = None):
+        if not self.session:
+            raise SessionNotCreated("Session is not initialized fully. Use a context manager when saving.")
 
+        webpage_url = self.modify_original_url(webpage_url)
+        media_urls = await self.get_media_urls(webpage_url)
 
-def get_image_sources_from_dom(soup: BeautifulSoup) -> Iterable[str]:
-    for img in soup.find_all("img"):
-        if img["src"] == "https://nstatic.dcinside.com/dc/m/img/dccon_loading_nobg200.png":  # loading image as placeholder before loading.
+        if not media_urls:
+            raise MediaNotFound
+
+        os.makedirs(output_directory, exist_ok=True)
+        for media_url in media_urls:
+            try:
+                async with self.session.get(media_url, headers=self.HEADERS) as res:
+                    filename = self.get_filename(res.headers)
+                    content = await res.content.read()
+            except aiohttp.ClientResponseError:
+                self.logger.exception(f"Error encountered when downloading {media_url}")
+            else:
+                self.save_to(content, output_directory, filename)
+
+    async def get_media_urls(self, webpage_url: UrlLike) -> list[UrlLike]:
+        soup = BeautifulSoup(await self.fetch_post_html(webpage_url), "lxml")
+        return list(self.get_image_sources_from_dom(soup))
+
+    @staticmethod
+    def get_image_sources_from_dom(soup: BeautifulSoup) -> Iterable[str]:
+        for img in soup.select("img[class='lazy'][data-original]"):
             yield img["data-original"]
 
+    async def fetch_post_html(self, webpage_url: UrlLike) -> bytes:
+        async with self.session.get(webpage_url, headers=self.HEADERS) as res:
+            res.raise_for_status()
+            if not res.ok:
+                self.logger.warning(f"Error occurred when fetching {webpage_url}. Error {res.status}")
 
-async def fetch_post_html(session: ClientSession, board_id: str, post_id: str) -> bytes:
-    async with session.get(
-        DCINSIDE_URL.format(
-            board_id,
-            post_id
-        ),
-        cookies=GALLERY_POSTS_COOKIES,
-        headers=HEADERS
-    ) as res:
-        res.raise_for_status()
-        return await res.content.read()
+            return await res.content.read()
 
+    def get_board_and_post_id(self, webpage_url: UrlLike) -> tuple[str, str]:
+        res = re.search(self.SITE_REGEX, webpage_url)
+        if not res:
+            raise NotDcinsideLink(f"{webpage_url} is not a dcinside link.")
+        return res[1], res[2]
 
-async def save_dcinside_media(url: str, out_dir: PathLike = "./dcinside_media") -> list[str]:
-    os.makedirs(out_dir, exist_ok=True)
+    def modify_original_url(self, webpage_url: UrlLike) -> UrlLike:
+        #  gall.dcinside returns a response that redirects to the m.dcinside endpoint. better off just using m.dcinside
+        board_id, post_id = self.get_board_and_post_id(webpage_url)
+        return self.SCRAPE_ENDPOINT.format(board_id, post_id)
 
-    async with aiohttp.ClientSession() as session:
-        board_id, post_id = get_board_post_id(url)
-        board_content = await fetch_post_html(session, board_id, post_id)
-
-        soup = BeautifulSoup(board_content, "lxml")
-
-        paths = []
-        for media_url in get_image_sources_from_dom(soup):
-            try:
-                paths.append(
-                    await download_source_url(session, media_url, out_dir=out_dir, filename=f"{board_id}_{post_id}")
-                )
-            except aiohttp.ClientResponseError:
-                logger.exception(f"Error encountered when downloading {media_url}")
-
-        if not paths:
-            raise MediaNotFound
-        return paths
+    @staticmethod
+    def get_filename(response_headers: Mapping) -> str:
+        # 'Content-Disposition': 'attachment; filename=1705115980.png'
+        return response_headers["Content-Disposition"].rsplit('=')[-1]
 
 
 if __name__ == "__main__":
@@ -84,4 +86,9 @@ if __name__ == "__main__":
     url = "https://gall.dcinside.com/mgallery/board/view/?id=projectmx&no=7579554"
     # url = "https://m.dcinside.com/board/projectmx/7579554"
 
-    asyncio.run(save_dcinside_media(url))
+    async def main():
+        async with Dcinside() as dcinside:
+            await dcinside.save(url)
+
+    asyncio.run(main())
+
