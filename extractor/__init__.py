0a2af9982ca54e23a53b72fd6fc8369651700d07
Poleric <nixonleongkb@gmail.com>
2024-01-19 22:05:53 +0800

    update __init__.py

--- extractor/__init__.py
+++ extractor/__init__.py
@@ -1,47 +1,39 @@
-import re
-import logging
 from extractor.exceptions import ExtractorError
-from extractor.twitter import TWITTER_REGEX, save_tweet_media
-from extractor.pixiv import PIXIV_REGEX, save_pixiv_media
-from extractor.dcinside import DCINSIDE_REGEX, save_dcinside_media
+from extractor.base import Extractor
+from extractor.twitter import Twitter
+from extractor.pixiv import Pixiv
+from extractor.dcinside import Dcinside
+
+from typing import Type
 
 
-logger = logging.getLogger(__name__)
+extractors: tuple[Type[Extractor], ...] = (
+    Twitter,
+    Pixiv,
+    Twitter
+)
 
 
 class NotValidQuery(ExtractorError):
     pass
 
 
-async def save_media(query: str, *, use_twitter=True, use_pixiv=True, use_dcinside=True, **kwargs) -> None:
+async def save_media(query: str) -> None:
     """
     Uses the correct extractor and saves the specified query / url.
 
     Args:
-        query: The url containing medias to be saved. Supported sites are: Twitter, Pixiv.
-        use_twitter:
-        use_pixiv:
-        use_dcinside:
-
-        kwargs:
-            twitter_out_dir: The directory to save Twitter media. Defaults to "./twitter_media"
-            pixiv_out_dir: The directory to save Pixiv media. Defaults to "./pixiv_media"
+        query: The url containing medias to be saved. Supported sites are: Twitter, Pixiv, dcinside.
 
     Raises:
         NotValidQuery: Raised when specified query is not in the supported sites.
         ScrapingException: Raised when an exception is raised while parsing and saving a media.
     """
 
-    if use_twitter and re.match(TWITTER_REGEX, query):
-        logger.info("Saving Twitter media.")
-        await save_tweet_media(query, **kwargs)
-        return
-    if use_pixiv and re.match(PIXIV_REGEX, query):
-        logger.info("Saving Pixiv media.")
-        await save_pixiv_media(query, **kwargs)
-        return
-    if use_dcinside and re.match(DCINSIDE_REGEX, query):
-        logger.info("Saving Dcinside media.")
-        await save_dcinside_media(query, **kwargs)
-        return
+    for extractor in extractors:
+        if extractor.check_link(query):
+            async with extractor() as source:
+                await source.save(query)
+            return
+
     raise NotValidQuery("The specified query type is not supported.")
