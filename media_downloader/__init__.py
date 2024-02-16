7c35caf4ab56af944ec37f0812abf5da39c4310b
Poleric <nixonleongkb@gmail.com>
2024-02-16 22:01:15 +0800

    fix import

--- media_downloader/__init__.py
+++ media_downloader/__init__.py
@@ -1,7 +1,7 @@
 from .exceptions import ExtractorError
 from .base import Extractor
 from .twitter import Twitter
-from media_downloader.pixiv.pixiv import Pixiv
+from .pixiv import Pixiv
 from .dcinside import Dcinside
 from .media import Media
 
