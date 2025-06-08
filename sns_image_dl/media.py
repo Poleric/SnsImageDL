b87a72237d843ed87b71711fed8ceb6314ebc36a
DizzyNight <laffey@dizzynight.moe>
2025-06-08 15:19:47 +0800

    make repr readable

--- sns_image_dl/media.py
+++ sns_image_dl/media.py
@@ -6,3 +6,9 @@ from sns_image_dl.metadata import Metadata
 class Media(NamedTuple):
     content: bytes
     metadata: Metadata
+
+    def __repr__(self):
+        return "Media(" \
+               f"content={self.content[:16] + b"..." if len(self.content) > 16 else self.content}, " \
+               f"metadata={self.metadata}" \
+               ")"
